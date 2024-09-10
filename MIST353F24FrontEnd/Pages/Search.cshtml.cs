fe5ffdf4b1eb42a18c9f380aa4a59a4b9b39f60f
jmb0035 <142920022+jmb0035@users.noreply.github.com>
2024-09-10 14:53:49 -0400

    added search page

---
+++ MIST353F24FrontEnd/Pages/Search.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST353F24FrontEnd.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
