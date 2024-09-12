383f153f5548a539b5be88a9a3c5394b0d3d959e
jmb0035 <142920022+jmb0035@users.noreply.github.com>
2024-09-12 15:02:15 -0400

    mid class push incase laptop kicks it

--- MIST353F24FrontEnd/wwwroot/js/site.js
+++ MIST353F24FrontEnd/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+ async function fetchInfo() {
+    let userInput = document.getElementById("inputFld").value;
+    let url = `https://world.openfoodfacts.org/api/v3/product/$(userInput).json`
+
+     const response = await fetch(url);
+     const data = await response.json();
+
+     let productNutrientLevels = data.product.nutrient_levels
+
+     for (let key in productNutrientLevels) {
+
+     }
+
+}
+
+
